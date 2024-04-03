57b8043595922932c82ec630d72f677fffbad8a0
Himanshurathore7023 <rathore.himanshu96@gmail.com>
2024-04-03 16:15:56 +0530

    10 project

--- tailwindProject/09themeSwicher/src/assets/context/Theme.js
+++ tailwindProject/09themeSwicher/src/assets/context/Theme.js
@@ -12,3 +12,6 @@ export const ThemeProvider=ThemeContex.Provider
 export default function useTheme(){      //coustom hook
        return useContext(ThemeContex)
 }
+
+// useContext:-
+// the context itself does not hold the information it only represent the kind of information you can provide or read from components
---
+++ tailwindProject/10todoProject/src/components/index.js
@@ -0,0 +1,6 @@
+import TodoFrom from "./TodoFrom";
+import TodoItem from "./TodoItem";
+
+
+
+export {TodoFrom,TodoItem} 
